64d2d279904a7024b9c25278b8e47592a5e3575d
John Rohb Paloma <palomajohnrohb@gmail.com>
2022-03-26 17:18:04 +0800

    Finished gameRound function

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function computerPlay() {
 
     return pick[index];
 }
+
+function gameRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        return "Tie!"
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == "scissors") {
+            return ("You Win! Rock beats Scissors")
+        } else {
+            return ("You Lose! Paper beats Rock")
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == "rock") {
+            return ("You Win! Paper beats Rock")
+        } else {
+            return ("You Lose! Scissors beats Paper")
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == "paper") {
+            return ("You Win! Scissors beats Paper")
+        } else {
+            return ("You Lose! Rock beats Scissors")
+        }
+    }
+}
